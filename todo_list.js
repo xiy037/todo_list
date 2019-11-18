0b2fe1c5ec0826a078684553bc691aab77fe93e9
Xiao Yu <xiao.susan.yu@gmail.com>
2019-11-19 01:44:56 +0800

    Simple Todo List with adding and filter by diff status.

---
+++ todo_list.js
@@ -0,0 +1,53 @@
+function addList() {
+  var inputText = document.getElementById("userInput").value;
+  var listContent = document.createTextNode(inputText);
+  var list = document.getElementById("list");
+  var newList = document.createElement("li");
+  var checkBox = document.createElement("input");
+  checkBox.setAttribute("type", "checkbox");
+  checkBox.setAttribute("class", "checkBoxClass");
+  newList.appendChild(listContent);
+  newList.insertBefore(checkBox, listContent);
+  list.appendChild(newList);
+  checkBox.addEventListener("click", markCheckBox);
+  clearDisplay();
+}
+
+function clearDisplay() {
+  document.getElementById("userInput").value = "";
+}
+
+function markCheckBox() {
+  var boxList = document.getElementsByClassName("checkBoxClass");
+  var listItem = document.getElementsByTagName("li");
+  for (i = 0; i < boxList.length; i++) {
+    if (boxList[i].checked) {
+      listItem[i].className = "complete";
+    } else {
+      listItem[i].className = "active";
+    }
+  }
+}
+
+function listAll() {
+  var listItem = document.getElementsByTagName("li");
+  for (i = 0; i < listItem.length; i++) {
+    document.getElementsByTagName("li")[i].style.display = "list-item";
+  }
+}
+
+function listActive() {
+  listAll();
+  var complete= document.getElementsByClassName("complete");
+  for (i = 0; i < complete.length; i++){
+    document.getElementsByClassName("complete")[i].style.display = "none";
+  }
+}
+
+function listComplete() {
+  listAll();
+  var active = document.getElementsByClassName("active");
+  for (i = 0; i < active.length; i++) {
+    document.getElementsByClassName("active")[i].style.display = "none";
+  }
+}
